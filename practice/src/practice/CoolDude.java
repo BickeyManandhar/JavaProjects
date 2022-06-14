1d744c34402746dc54c1b911b963bfa892f641d2
BickeyManandhar <manandharbickey@gmail.com>
2022-06-14 15:32:11 -0400

    First Commit

--- practice/src/practice/CoolDude.java
+++ practice/src/practice/CoolDude.java
@@ -5,7 +5,7 @@ public class CoolDude {
 	public static void main(String[] args) {
 		for(int i=100;i<=500; i++) {
 			if(i%7==0 && i%11==0) {
-				System.out.println("Cool Dude");
+				System.out.println("Cool Dudee");
 			}
 			else if(i%7==0) {
 				System.out.println("Cool");
